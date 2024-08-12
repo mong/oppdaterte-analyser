53ab41f78afcad1349fb52c3a44307395110ca7d
Jo Inge Arnes <jo.i.arnes@uit.no>
2024-08-12 15:37:14 +0200

    added a header and box, just to see how it looked

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,18 @@
+import Box from "@mui/material/Box/Box";
 import Link from "@mui/material/Link";
 import Typography from "@mui/material/Typography";
 import Grid from "@mui/material/Unstable_Grid2";
+import Divider from "@mui/material/Divider";
 
 export default function Home() {
   return (
     <main>
       <Grid container spacing={2}>
         <Grid xs={12}>
-          <Typography variant="body1">
-            Se side for <Link href="/kompendium">kompendium</Link>
-          </Typography>
+          <Typography variant="h2">Oversiktsside</Typography>
+          <Box>
+            Se side for <Link href="/kompendium">kompendium</Link>.
+          </Box>
         </Grid>
       </Grid>
     </main>
--- src/app/template.tsx
+++ src/app/template.tsx
@@ -1,6 +1,7 @@
 import Footer from "./components/Footer";
 import PageWrapper from "./components/PageWrapper";
 
+// The template wraps the page content, and is itself is wrapped by the layout
 export default function MainTemplate({
   children,
 }: Readonly<{
