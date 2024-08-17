44133283db255c287526cec715fffa56dbff2ef8
jo-inge-arnes <jo.inge.arnes@helse-nord.no>
2024-08-17 10:50:19 +0200

    changing example chart a little

--- src/app/components/ResultBoxBarchart/index.tsx
+++ src/app/components/ResultBoxBarchart/index.tsx
@@ -1,19 +1,26 @@
 "use client";
-import * as React from "react";
-import { BarChart } from "@mui/x-charts/BarChart";
+import { ResponsiveChartContainer } from "@mui/x-charts/ResponsiveChartContainer";
+import { BarPlot } from "@mui/x-charts/BarChart";
+import { ChartsXAxis } from "@mui/x-charts/ChartsXAxis";
+import Paper from "@mui/material/Paper";
+import { ThemeContext } from "@emotion/react";
 
 export default function ChartsOverviewDemo() {
   return (
-    <BarChart
-      series={[
-        { data: [35, 44, 24, 34] },
-        { data: [51, 6, 49, 30] },
-        { data: [15, 25, 30, 50] },
-        { data: [60, 50, 15, 25] },
-      ]}
-      height={290}
-      xAxis={[{ data: ["Q1", "Q2", "Q3", "Q4"], scaleType: "band" }]}
-      margin={{ top: 10, bottom: 30, left: 40, right: 10 }}
-    />
+    <Paper sx={{ width: "100%", height: 300 }} elevation={3}>
+      <ResponsiveChartContainer
+        series={[{ type: "bar", data: [35, 44, 24, 34, 40] }]}
+        xAxis={[
+          {
+            data: ["A", "B", "C", "D", "E"],
+            scaleType: "band",
+            id: "x-axis-id",
+          },
+        ]}
+      >
+        <BarPlot margin={{ top: 10, bottom: 10, left: 40, right: 40 }} />
+        <ChartsXAxis label="X axis" position="bottom" axisId="x-axis-id" />
+      </ResponsiveChartContainer>
+    </Paper>
   );
 }
