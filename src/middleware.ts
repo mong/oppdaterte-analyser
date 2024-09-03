6a4cfa104faeb86436637d81ffbe8f524ef35f8d
Mattias Ugelvik <mattias.ugelvik@helse-nord.no>
2024-09-03 12:22:13 +0200

    Forenklet middleware til å bare gjelde /

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,15 +1,9 @@
-import { NextResponse } from "next/server";
-import type { NextRequest } from "next/server";
+import { NextResponse, NextRequest } from "next/server";
 
 export function middleware(request: NextRequest) {
-  const url = request.nextUrl.clone();
-  url.pathname = `/no${request.nextUrl.pathname}`;
-  return NextResponse.redirect(url);
+  return NextResponse.rewrite(new URL("/no/", request.url));
 }
 
 export const config = {
-  matcher: [
-    // Hvis ugyldig språk i URL, legger vi til /no/
-    "/((?!_next|no/|en/|img/|favicon.ico).*)",
-  ],
+  matcher: ["/"],
 };
