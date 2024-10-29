5f37d03d9d364e7b4a390d6514397bd103dc96d1
Mattias Ugelvik <mattias.ugelvik@helse-nord.no>
2024-10-29 14:08:36 +0100

    Ga en fast farge til hvert område i linjediagram

--- src/components/AnalyseBox/AnalyseLineChart.tsx
+++ src/components/AnalyseBox/AnalyseLineChart.tsx
@@ -3,6 +3,43 @@ import { LineChart } from "@mui/x-charts/LineChart";
 import { regions_dict, Selection } from "@/lib/nameMapping";
 import React from "react";
 
+const linechart_colors: {
+  sykehus: { [k: number]: string };
+  region: { [k: number]: string };
+} = {
+  sykehus: {
+    1: "#C3A687",
+    2: "#B8FFF5",
+    3: "#003283",
+    4: "#2F654A",
+    6: "#F38411",
+    7: "#42E0F5",
+    8: "#81BD00",
+    10: "#839C8F",
+    11: "#5C3229",
+    12: "#11F063",
+    13: "#6B9B3A",
+    14: "#D9CB68",
+    15: "#BD0C2E",
+    16: "#C5F542",
+    17: "#E3A611",
+    18: "#EF42F5",
+    19: "#F0116E",
+    20: "#8D6A59",
+    21: "#101010",
+    22: "#81A9E1",
+    23: "#FFA3EB",
+    8888: "#9AA2AB",
+  },
+  region: {
+    1: "#C5F542",
+    2: "#F38411",
+    3: "#81A9E1",
+    4: "#BD0C2E",
+    8888: "#9AA2AB",
+  },
+};
+
 type AnalyseLineChartProps = {
   analyse: Analyse;
   years: number[];
@@ -70,9 +107,10 @@ export const AnalyseLineChart = ({
       series={selectionIDs.map((area) => ({
         dataKey: area,
         id: area,
-        curve: "linear",
+        curve: "monotoneX",
         showMark: false,
         label: regions_dict[lang][level][Number(area)],
+        color: linechart_colors[level][Number(area)],
       }))}
       onLineClick={(a, b) => console.log("Info: ", a, b)}
       tooltip={{ trigger: "axis" }}
