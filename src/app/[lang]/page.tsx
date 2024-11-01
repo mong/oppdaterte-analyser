60077ef4d0ea7961154a45d0479e447fd255660c
Mattias Ugelvik <mattias.ugelvik@helse-nord.no>
2024-11-01 08:55:54 +0100

    Fikset oversettelse av HTML metadata for forsiden

--- src/app/[lang]/page.tsx
+++ src/app/[lang]/page.tsx
@@ -3,11 +3,25 @@ import Typography from "@mui/material/Typography";
 import Header from "@/components/Header";
 import { Box } from "@mui/material";
 import { Lang } from "@/types";
+import { getDictionary } from "@/lib/dictionaries";
 
 export const dynamicParams = false;
 export async function generateStaticParams() {
   return [{ lang: "no" }, { lang: "en" }];
 }
+
+export const generateMetadata = async ({
+  params,
+}: {
+  params: { lang: Lang };
+}) => {
+  const dict = await getDictionary(params.lang);
+
+  return {
+    title: dict.frontpage.title,
+    description: dict.frontpage.description,
+  };
+};
 
 export type MainPageProps = {
   params: {
