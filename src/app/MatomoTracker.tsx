080d02eb4e5c40ab10c7b22cf84af01b5ff99c92
Mattias Ugelvik <mattias.ugelvik@helse-nord.no>
2024-12-09 13:24:30 +0100

    Fjerner feilmeldinger

--- src/app/MatomoTracker.tsx
+++ src/app/MatomoTracker.tsx
@@ -2,6 +2,10 @@
 
 import React from "react";
 
+declare const window: {
+  _mtm: any;
+} & Window;
+
 export default function MatomoTracker() {
   React.useEffect(() => {
     const _mtm = (window._mtm = window._mtm || []);
@@ -11,7 +15,7 @@ export default function MatomoTracker() {
       s = d.getElementsByTagName("script")[0];
     g.async = true;
     g.src = "https://statistikk.fnsp.no/js/container_wcHdDuAW.js";
-    s.parentNode.insertBefore(g, s);
+    s.parentNode && s.parentNode.insertBefore(g, s);
   }, []);
   return <></>;
 }
