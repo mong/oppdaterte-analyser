0b88d528609adea73e5961b437d5b8c54f3771a8
Mattias Ugelvik <mattias.ugelvik@helse-nord.no>
2025-02-17 09:18:25 +0100

    Fikser litt på URL i sitemap.xml

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -15,34 +15,34 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
         .reduce((acc, val) => (acc > val ? acc : val)),
       alternates: {
         languages: {
-          en: "https://analyser.skde.no/en",
+          en: "https://analyser.skde.no/en/",
         },
       },
     },
   ]
     .concat(
       kompendier.map((komp) => ({
-        url: `https://analyser.skde.no/no/${komp.name}`,
+        url: `https://analyser.skde.no/no/${komp.name}/`,
         lastModified: analyser
           .filter((analyse) => analyse.tags.includes(komp.name))
           .map((analyse) => analyse.updatedAt)
           .reduce((acc, val) => (acc > val ? acc : val)),
         alternates: {
           languages: {
-            en: `https://analyser.skde.no/en/${komp.name}`,
-            no: `https://analyser.skde.no/no/${komp.name}`,
+            en: `https://analyser.skde.no/en/${komp.name}/`,
+            no: `https://analyser.skde.no/no/${komp.name}/`,
           },
         },
       })),
     )
     .concat(
       analyser.map((analyse) => ({
-        url: `https://analyser.skde.no/no/analyse/${analyse.name}`,
+        url: `https://analyser.skde.no/no/analyse/${analyse.name}/`,
         lastModified: new Date(analyse.updatedAt),
         alternates: {
           languages: {
-            en: `https://analyser.skde.no/en/analyse/${analyse.name}`,
-            no: `https://analyser.skde.no/no/analyse/${analyse.name}`,
+            en: `https://analyser.skde.no/en/analyse/${analyse.name}/`,
+            no: `https://analyser.skde.no/no/analyse/${analyse.name}/`,
           },
         },
       })),
