04288d98be899a7bf2d7821364aba9213c18865e
Mattias Ugelvik <mattias.ugelvik@helse-nord.no>
2025-03-10 13:52:52 +0100

    Glemte å fjerne ubrukt api-endpoint

--- src/app/api/publish/route.ts
+++
@@ -1,7 +0,0 @@
-import { publishAnalyse } from "@/services/mongo";
-
-export async function POST(request: Request) {
-  const analyseName = await request.text();
-
-  return publishAnalyse(analyseName);
-}
