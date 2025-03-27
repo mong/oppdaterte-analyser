2375ff2f2f651a3b61e3d13f29a045354491f735
Mattias Ugelvik <mattias.ugelvik@helse-nord.no>
2025-03-27 12:11:52 +0100

    La til en redirect for å fikse et problem i FNSP

--- next.config.mjs
+++ next.config.mjs
@@ -9,6 +9,12 @@ const nextConfig = {
             destination: '/.auth/login/cognito',
             permanent: true,
           },
+          { // Denne redirecten er lagt til som en fiks for problemer i FNSP. Problemet er at vi vil ha et
+            // forsidekort med link til samlesiden for alle helseatlas, uten å få med banner-bildet til skde.no/helseatlas/
+            source: '/helseatlas-forside/',
+            destination: 'https://www.skde.no/helseatlas/',
+            permanent: true,
+          }
         ]
       },
 };
