9c37469f76feb8b65599867b5277ec35fc6cf928
Uglemat <mattias.ugelvik@helse-nord.no>
2026-03-13 10:34:28 +0100

    Fikset iframe til klinisk dashbord

--- src/app/(frontend)/klinisk-dashbord/page.tsx
+++ src/app/(frontend)/klinisk-dashbord/page.tsx
@@ -30,13 +30,13 @@ export default async function Page() {
         <Typography variant="h6" className="m-8">
         </Typography>
       </Header>
-      <Container maxWidth="xxl">
+      <Container maxWidth="xxl" disableGutters>
         <iframe
           title="Klinisk Dashboard vNext"
-          width="1024"
-          height="1060"
-          className="border-0 py-8"
-          src="https://app.powerbi.com/view?r=eyJrIjoiYTViMDkwNjAtYjZjMS00NjQ5LWJiNzktMTYyN2VhOTNmMjk1IiwidCI6IjY3NzE4MTA4LTJlYjctNDc0Yy1hMWQ3LTQxNjU1ZDRiMWU2MSIsImMiOjh9%22"
+          width="1290"
+          height="850"
+          className="border-0 py-8 w-full"
+          src="https://app.powerbi.com/view?r=eyJrIjoiYTViMDkwNjAtYjZjMS00NjQ5LWJiNzktMTYyN2VhOTNmMjk1IiwidCI6IjY3NzE4MTA4LTJlYjctNDc0Yy1hMWQ3LTQxNjU1ZDRiMWU2MSIsImMiOjh9&pageName=f49200f896b02bdf618d"
           allowFullScreen={true}>
         </iframe>
       </Container>
